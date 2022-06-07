0d4fffa509abfcd044f82718209b4c889a9a8241
Guilherme Almeida <guimathiasgui@gmail.com>
2022-06-07 19:31:20 -0300

    Creating and Editing student

--- src/pages/Student/index.jsx
+++ src/pages/Student/index.jsx
@@ -1,11 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { get } from 'lodash';
+import PropTypes from 'prop-types';
 
 import { Container } from '../../styles/GlobalStyles';
+import { Form } from './styled';
 
-export default function Student() {
+export default function Student({ match }) {
+  const id = get(match, 'params.id', 0);
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
+  const [email, setEmail] = useState('');
+  const [age, setAge] = useState('');
+  const [weight, setWeight] = useState('');
+  const [height, setHeight] = useState('');
+
+  function handleSubmit(e) {
+    e.prevetDefault();
+  }
+
   return (
     <Container>
-      <h1>Student</h1>
+      <h1>{id ? 'Edit Student' : 'New Student'}</h1>
+
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Name"
+        />
+        <input
+          type="text"
+          value={surname}
+          onChange={(e) => setSurname(e.target.value)}
+          placeholder="Surname"
+        />
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="E-mail"
+        />
+        <input
+          type="number"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+          placeholder="Age"
+        />
+        <input
+          type="text"
+          value={weight}
+          onChange={(e) => setWeight(e.target.value)}
+          placeholder="Weight"
+        />
+        <input
+          type="text"
+          value={height}
+          onChange={(e) => setHeight(e.target.value)}
+          placeholder="Height"
+        />
+
+        <button type="submit">{id ? 'Save' : 'Create Student'}</button>
+      </Form>
     </Container>
   );
 }
+
+Student.propTypes = {
+  match: PropTypes.shape({}).isRequired,
+};
--- src/pages/Students/index.jsx
+++ src/pages/Students/index.jsx
@@ -10,7 +10,7 @@ import {
 } from 'react-icons/fa';
 
 import { Container } from '../../styles/GlobalStyles';
-import { StudentContainer, ProfilePicture } from './styled';
+import { StudentContainer, ProfilePicture, NewStudent } from './styled';
 import axios from '../../services/axios';
 
 import Loading from '../../components/Loading';
@@ -65,6 +65,8 @@ export default function Students() {
       <Loading isLoading={isLoading} />
 
       <h1>Students</h1>
+
+      <NewStudent to="/student/">New Student</NewStudent>
 
       <StudentContainer>
         {students.map((student, index) => (
